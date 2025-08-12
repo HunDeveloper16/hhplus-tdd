626372f66419268a3733276e53332608f3b59435
shlee <aba7500@naver.com>
2025-08-12 17:54:28 +0900

    feat : 포인트 사용 로직 ReentrantLock 추가

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +19,8 @@ public class PointService {
     private final PointHistoryTable pointHistoryTable;
 
     private static final Logger log = LoggerFactory.getLogger(PointService.class);
+
+    private final ReentrantLock lock = new ReentrantLock();
 
     /**
      * 유저 포인트를 조회합니다.
@@ -79,7 +82,21 @@ public class PointService {
      *
      * @param id 유저 아이디
      * @param amount 사용 포인트
+     *
      * @return 사용 이후 유저 포인트 정보
+     *
+     * 동시성 문제 :
+     *
+     * 유저가 두번의 포인트를 사용한다고 가정합니다.
+     * 유저는 총 1,000포인트를 가지고 있고 첫 번째 시도에선 500포인트를, 다음 시도에선 1000포인트를 사용하고자 합니다.
+     *
+     * 1.첫번째 시도에서 유저는 포인트 검증을 완료하고 insertOrUpdate()에서 3초의 시간을 sleep합니다.
+     * 2.바로 다음 두번째 시도에서 위 3초가 지나기전 포인트 검증을 완료하고, insertOrUpdate() 또 시도하려합니다.
+     * 3.따라서 모두 검증을 완료한 뒤 유저의 최종 포인트는 -500 포인트가 되어있을 것입니다.
+     *
+     * 이에 따라, ReentrantLock을 사용하여 문제가 될 수 있는 로직에서 lock을 걸고자 합니다.
+     * lock()내부에는 최소한의 문제가 될 수 있는 로직만 포함해야 하기 때문에 내역을 쌓는 로직은 제외하였습니다.
+     *
      */
     public UserPoint useUserPoint(long id, long amount) {
         if(amount < 0) {
@@ -87,29 +104,36 @@ public class PointService {
             throw new CustomException("음수의 포인트는 사용될 수 없습니다.");
         }
 
-        // 1.유저 조회
-        UserPoint userPoint = userPointTable.selectById(id);
+        UserPoint updatedUserPoint;
 
-        // 사용 이후 포인트 계산
-        long updatedPoints = userPoint.point() - amount;
-
-        if(userPoint.point() - amount < 0){
-            log.error("소유한 포인트보다 사용 포인트가 더 큽니다. 현재 계좌: {} , 사용 포인트: {}", userPoint.point(), amount);
-            throw new CustomException("소유한 포인트보다 사용 포인트가 더 큽니다.");
-        }
-
-        // 2. 유저 포인트 갱신
+        lock.lock();
         try{
-            userPoint = userPointTable.insertOrUpdate(id, updatedPoints);
-        }catch (Exception e){
-            log.error("포인트 사용 중 에러가 발생하였습니다.");
-            throw new CustomException("포인트 충전 중 DB에러 발생.");
+            // 1.유저 조회
+            UserPoint userPoint = userPointTable.selectById(id);
+            // 사용 이후 포인트 계산
+            long updatedPoints = userPoint.point() - amount;
+
+            if(userPoint.point() - amount < 0){
+                log.error("소유한 포인트보다 사용 포인트가 더 큽니다. 현재 계좌: {} , 사용 포인트: {}", userPoint.point(), amount);
+                throw new CustomException("소유한 포인트보다 사용 포인트가 더 큽니다.");
+            }
+
+            // 2. 유저 포인트 갱신
+            try{
+                updatedUserPoint = userPointTable.insertOrUpdate(id, updatedPoints);
+            }catch (Exception e){
+                log.error("포인트 사용 중 에러가 발생하였습니다.");
+                throw new CustomException("포인트 충전 중 DB에러 발생.");
+            }
+        } finally {
+            lock.unlock();
         }
 
         // 3. 내역 추가
-        pointHistoryTable.insert(id, updatedPoints, TransactionType.USE, System.currentTimeMillis());
+        pointHistoryTable.insert(id, updatedUserPoint.point(), TransactionType.USE, System.currentTimeMillis());
 
-        return userPoint;
+        return updatedUserPoint;
+
     }
 
 
